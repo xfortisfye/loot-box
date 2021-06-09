953a0c30972a94e044dcc9aa147398254ef688c5
ehandywhyy <52729376+ehandywhyy@users.noreply.github.com>
2021-06-09 14:35:34 +0800

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,6 @@
+# remove git lfs
+# https://stackoverflow.com/questions/35011366/move-git-lfs-tracked-files-under-regular-git/54119191#54119191
+
+# print numbers in decimal places without rounding up or down
+def truncate(f, n):
+    return floor(f * 10 ** n) / 10 ** n
